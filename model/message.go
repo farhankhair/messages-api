e3f50e053f9976646d0e4b8956d78896274204f7
Farhan Ramadhan <farhanramadhansk@gmail.com>
2020-03-14 11:50:23 +0700

    create model for message

---
+++ model/message.go
@@ -0,0 +1,30 @@
+package model
+
+import (
+	"github.com/google/uuid"
+)
+
+// Message struct
+type Message struct {
+	ID   uuid.UUID
+	Body string
+}
+
+// NewMessage is a constructor for Message
+func NewMessage(body string) *Message {
+	message := &Message{
+		ID:   uuid.New(),
+		Body: body,
+	}
+	return message
+}
+
+// GetID :nodoc:
+func (m *Message) GetID() uuid.UUID {
+	return m.ID
+}
+
+// GetBody :nodoc:
+func (m *Message) GetBody() string {
+	return m.Body
+}
