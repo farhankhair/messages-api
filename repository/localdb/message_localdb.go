883d9102f4fa214c4c0be3b105bd175efdca39a6
Farhan Khair <farhanramadhansk@gmail.com>
2022-07-03 21:57:21 +0700

    private repo

--- repository/localdb/message_localdb.go
+++ repository/localdb/message_localdb.go
@@ -3,25 +3,25 @@ package localdb
 import "github.com/farhanramadhan/messages-api/model"
 
 // Repo is a temporary database
-type Repo struct {
+type repo struct {
 	data []model.Message
 }
 
 // NewLocalDBRepo is a constructor
-func NewLocalDBRepo() *Repo {
-	repo := &Repo{
+func NewLocalDBRepo() *repo {
+	repo := &repo{
 		data: make([]model.Message, 0),
 	}
 	return repo
 }
 
 // GetAllMessages is to get all messages
-func (lc *Repo) GetAllMessages() []model.Message {
+func (lc *repo) GetAllMessages() []model.Message {
 	return lc.data
 }
 
 // InsertMessage is to insert a message
-func (lc *Repo) InsertMessage(message model.Message) error {
+func (lc *repo) InsertMessage(message model.Message) error {
 	lc.data = append(lc.data, message)
 	return nil
 }
